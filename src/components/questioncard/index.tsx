bb105463b4de53e141fc6474f6613c4894d4804a
Yuette Yoko <yuetteyo@student.21-school.ru>
2024-05-26 23:40:26 +0300

    fix: questioncard is fixed

--- src/components/questioncard/index.tsx
+++ src/components/questioncard/index.tsx
@@ -1,40 +1,55 @@
-import {Box, Heading, Flex} from "@chakra-ui/react"
-import React from "react"
-import {QuestionCardProps} from "@/interface/index"
-import Appbutton from "@/components/button"
+import React, { MouseEvent } from 'react';
 
-const QuestionCard:  React.FC<QuestionCardProps>= ({
+import Appbutton from '@/components/button';
+import { Box, Flex, Heading } from '@chakra-ui/react';
+
+interface QuestionCardProps {
+    questions: string;
+    category: string;
+    totalQuestions?: number;
+    questionNumber: number; //?:
+    callback: (e: MouseEvent<HTMLButtonElement>) => void; // Function
+}
+
+const QuestionCard: React.FC<QuestionCardProps> = ({
     questions,
-    callback, 
+    callback,
     category,
     totalQuestions,
     questionNumber
 }) => {
     return (
         <>
-        <Box bg="white" width="100%">
-            <Box mb={6} fontSize={"md"} fontWeight={"bold"} textTransform={"uppercase"}>
-                Your progress: {questionNumber}/{totalQuestions}
-            </Box>
-            <Box>
-                {category}
-            </Box>
-            <Heading>
-                <Box>{questions}</Box>
-            </Heading>
-
-            <Flex>
-                <Box>
-                    <Appbutton
-                        colorScheme="purple"
-                        variant="outline"
-                        value="False"
-                        width="full"
-                        onClick={callback}
-                    />
+            <Box bg="white" width="100%">
+                <Box mb={6} fontSize={"md"} fontWeight={"bold"} textTransform={"uppercase"}>
+                    Your progress: {questionNumber}/{totalQuestions}
                 </Box>
-            </Flex>
-        </Box>
+                <Box>
+                    {category}
+                </Box>
+                <Heading>
+                    <Box>{questions}</Box>
+                </Heading>
+
+                <Flex>
+                    <Box>
+                        <Appbutton
+                            colorScheme="purple"
+                            variant="outline"
+                            value="False"
+                            width="full"
+                            onClick={callback}
+                        />
+                        <Appbutton
+                            colorScheme="purple"
+                            variant="outline"
+                            value="True"
+                            width="full"
+                            onClick={callback}
+                        />
+                    </Box>
+                </Flex>
+            </Box>
         </>
     )
 }
