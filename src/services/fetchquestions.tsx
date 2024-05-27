8eba3fb05e61ee984c857b125c44d17dbd1675f5
Yuette Yoko <yuetteyo@student.21-school.ru>
2024-05-27 12:45:26 +0300

    fix: added callback func into fetchquestion

--- src/services/fetchquestions.tsx
+++ src/services/fetchquestions.tsx
@@ -1,17 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
 import { Questions } from '@/interface/questions.interface';
 
-const baseURL = "https://opentdb.com/api.php?amount=10&type=boolean";
+const baseURL = "https://opentdb.com/api.php?amount=10";
 
 
 // amount: number,
 // difficulty: string,
 export const getQuestionList = async (
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>,
 ) => {
     try {
         // const response = await axios.get(`${baseURL}${amount}&difficulty=${difficulty}&type=boolean`);
+        setLoading(true);
         const { data } = await axios.get<Questions>(baseURL);
+        setLoading(false);
         return data;
     }
     catch (error) {
